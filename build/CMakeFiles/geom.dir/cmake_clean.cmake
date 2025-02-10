0423df2f3a16b68c094770e7a859f74fd8dc91f1
Ershui1 <139448005+Ershui1@users.noreply.github.com>
2025-02-10 17:59:53 -0500

    Add files via upload

---
+++ build/CMakeFiles/geom.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/geom.dir/bin/geom.cpp.obj"
+  "CMakeFiles/geom.dir/bin/geom.cpp.obj.d"
+  "geom.exe"
+  "geom.exe.manifest"
+  "geom.pdb"
+  "libgeom.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/geom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/text.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/text.dir/bin/text.cpp.obj"
+  "CMakeFiles/text.dir/bin/text.cpp.obj.d"
+  "libtext.dll.a"
+  "text.exe"
+  "text.exe.manifest"
+  "text.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/text.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
