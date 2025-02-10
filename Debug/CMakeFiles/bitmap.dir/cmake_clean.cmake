d5451787617e5f01291b6f8224b4bdf2cc327f64
Ershui1 <139448005+Ershui1@users.noreply.github.com>
2025-02-10 03:34:11 -0500

    Add files via upload

---
+++ Debug/CMakeFiles/bitmap.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bitmap.dir/bin/bitmap.cpp.obj"
+  "CMakeFiles/bitmap.dir/bin/bitmap.cpp.obj.d"
+  "bitmap.exe"
+  "bitmap.exe.manifest"
+  "bitmap.pdb"
+  "libbitmap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bitmap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
